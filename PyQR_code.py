50fa09e035fc3890e75115c7e6e049916c4cb652
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-03-07 10:13:18 +0100

    QR_code_generator

---
+++ PyQR_code.py
@@ -0,0 +1,12 @@
+import pyqrcode
+from pyqrcode import QRCode
+
+
+# String to represent the QR code
+str_ = "http://idoston.com/python-development"
+
+# Generate QR Code
+url = pyqrcode.create(str_)
+
+# Create and save the svg file with name idostoncom
+url.svg("idostoncom.svg", scale=8)
