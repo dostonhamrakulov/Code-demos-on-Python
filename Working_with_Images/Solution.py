4cfede521bdae0d0ffaa09507fd937e620f26806
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-03-08 17:43:33 +0100

    3 operations are done

--- Working_with_Images/Solution.py
+++ Working_with_Images/Solution.py
@@ -1,9 +1,21 @@
 from PIL import Image
 
+try:
+    #1.  Opening image from specific path
+    filename = "image.png"
+    with Image.open(filename) as image:
+        #2. retrieving information about image
+        width, height = image.size
 
-filename = "image.png"
-with Image.open(filename) as image:
-    width, height = image.size
+        # Rotating image with given angle
+        img = image.rotate(180)
+
+        # Saved in the same relative location
+        img.save("rotated_picture.jpg")
+except IOError:
+    pass
 
 print("Width: " + str(width))
 print("Height: " + str(height))
+
+#3.
